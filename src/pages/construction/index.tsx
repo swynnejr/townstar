cf05f2cf8d5d5b646875b7dba54112a3fab9d683
Sam Wynne <saucersam@gmail.com>
2023-02-06 17:39:45 -0600

    Google Sheets API

---
+++ src/pages/construction/index.tsx
@@ -0,0 +1,44 @@
+import { google } from "googleapis";
+import type { GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const auth = await google.auth.getClient({
+    scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+  });
+
+  const sheets = google.sheets({ version: "v4", auth });
+
+  const { id }: string | string[] = query;
+  const range = "Sheet4!B2:AJ1356";
+
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SHEET_ID,
+    range,
+  });
+
+  const [title, content] = response.data.values[0];
+  console.log(title, content);
+
+  return {
+    props: {
+      title,
+      content,
+    },
+  };
+};
+
+type Props = {
+  title: string;
+  content: string;
+};
+
+function index({ title, content }: Props) {
+  return (
+    <article>
+      <h1>{title}</h1>
+      <div>{content}</div>
+    </article>
+  );
+}
+
+export default index;
